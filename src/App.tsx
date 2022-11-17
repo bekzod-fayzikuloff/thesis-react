267897c30eb8113395a20c1e60a76026cb68a709
bekzod-fayzikuloff <fayzikuloff.b@gmail.com>
2022-11-18 02:16:39 +0300

    :construction_worker: lint autocheker

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
 
 function App() {
   const url = `http://127.0.0.1:8000/api/chats/`;
-  const [text, setText]: any = useState('');
-
-  useEffect(() => {
-    axios.get(url).then((response) => setText(response.data));
-  }, [url]);
 
   return (
     <div className="App">
-      <h1>{text}</h1>
+      <h1>{url}</h1>
     </div>
   );
 }
