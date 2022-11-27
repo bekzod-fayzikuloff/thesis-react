db020e2820fc79c948f297effafdc7deecd95b6f
Fayzikulov Bekzod <fayzikuloff.b@gmail.com>
2022-11-28 02:05:13 +0300

    :sparkles: Typeset a register page template

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,53 @@
+import style from './Login.module.scss';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+function RegisterFormSide() {
+  const navigate = useNavigate();
+  return (
+    <div className={style.right__side}>
+      <div className={style.buttons__sections}>
+        <div>
+          <Button
+            style={{ backgroundColor: '#343A40', borderRadius: '11px' }}
+            variant="contained"
+            onClick={() => navigate('/login')}>
+            Login
+          </Button>
+          <Button
+            style={{ backgroundColor: '#0055FF', borderRadius: '11px' }}
+            variant="contained"
+            onClick={() => navigate('/register')}>
+            Register
+          </Button>
+        </div>
+      </div>
+
+      <LoginForm />
+      <p className={style.form__underline}>
+        Already have account
+        <span style={{ color: '#1E74FD', fontWeight: 'bold' }} onClick={() => navigate('/login')}>
+          {' '}
+          Login
+        </span>
+      </p>
+    </div>
+  );
+}
+
+function LoginForm() {
+  return (
+    <div>
+      <h1>Hess</h1>
+    </div>
+  );
+}
+
 export function LoginPage() {
-  return <h1>Login Page</h1>;
+  return (
+    <main className={style.register__root}>
+      <div className={style.left__side}></div>
+      <RegisterFormSide />
+    </main>
+  );
 }
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,3 +1,128 @@
+import style from './Register.module.scss';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LockIcon from '@mui/icons-material/Lock';
+import { Button, Checkbox, FormControlLabel } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+function RegisterFormSide() {
+  const navigate = useNavigate();
+  return (
+    <div className={style.right__side}>
+      <div className={style.buttons__sections}>
+        <div>
+          <Button
+            style={{ backgroundColor: '#343A40', borderRadius: '11px' }}
+            variant="contained"
+            onClick={() => navigate('/login')}>
+            Login
+          </Button>
+          <Button
+            style={{ backgroundColor: '#0055FF', borderRadius: '11px' }}
+            variant="contained"
+            onClick={() => navigate('/register')}>
+            Register
+          </Button>
+        </div>
+      </div>
+
+      <RegisterForm />
+      <p className={style.form__underline}>
+        Already have account
+        <span style={{ color: '#1E74FD', fontWeight: 'bold' }} onClick={() => navigate('/login')}>
+          {' '}
+          Login
+        </span>
+      </p>
+    </div>
+  );
+}
+
+function RegisterForm() {
+  const navigate = useNavigate();
+  return (
+    <div
+      className={style.register__form_r}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%'
+      }}>
+      <p className={style.form__headline}>Create your account</p>
+      <Box
+        className={style.form__box}
+        sx={{
+          width: '30%',
+          '& > :not(style)': { m: 1 }
+        }}>
+        <Input
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+          fullWidth={true}
+        />
+        <br />
+        <Input
+          id="input-mail"
+          startAdornment={
+            <InputAdornment position="start">
+              <MailOutlineIcon />
+            </InputAdornment>
+          }
+          fullWidth={true}
+        />
+        <br />
+        <Input
+          id="input-password"
+          startAdornment={
+            <InputAdornment position="start">
+              <LockIcon />
+            </InputAdornment>
+          }
+          type={'password'}
+          fullWidth={true}
+        />
+        <br />
+        <Input
+          id="input-password-confirm"
+          startAdornment={
+            <InputAdornment position="start">
+              <LockIcon />
+            </InputAdornment>
+          }
+          type={'password'}
+          fullWidth={true}
+        />
+        <br />
+        <FormControlLabel
+          style={{ color: '#808383' }}
+          control={<Checkbox style={{ fontSize: '12px' }} name="term" />}
+          label="Accept Term and Conditions"
+        />
+        <Button
+          style={{ backgroundColor: '#343A40', borderRadius: '7px' }}
+          variant="contained"
+          fullWidth={true}
+          onClick={() => navigate('/login')}>
+          Register
+        </Button>
+      </Box>
+    </div>
+  );
+}
+
 export function RegisterPage() {
-  return <h1>Register Page</h1>;
+  return (
+    <main className={style.register__root}>
+      <div className={style.left__side}></div>
+      <RegisterFormSide />
+    </main>
+  );
 }
