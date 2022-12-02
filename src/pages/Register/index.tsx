179222f18829d618144443dc8969f44808ab4be9
Fayzikulov Bekzod <fayzikuloff.b@gmail.com>
2022-12-02 21:08:58 +0300

    Add Register page validators

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -5,12 +5,18 @@ import InputAdornment from '@mui/material/InputAdornment';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import LockIcon from '@mui/icons-material/Lock';
-import { Alert, Button, Checkbox, FormControlLabel } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useRef, useState } from 'react';
-import { API_URL } from '../../config';
+import {Alert, Button, Checkbox, FormControlLabel} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
+import {useRef, useState} from 'react';
+import {API_URL} from '../../config';
 import axios from 'axios';
-import { InfinitySpin } from 'react-loader-spinner';
+import {InfinitySpin} from 'react-loader-spinner';
+import {
+  passwordSameValidate,
+  validateEmail,
+  validatePassword,
+  validateUsername
+} from "../../services/utils/validators";
 
 function RegisterFormSide() {
   const navigate = useNavigate();
@@ -19,14 +25,14 @@ function RegisterFormSide() {
       <div className={style.buttons__sections}>
         <div>
           <Button
-            style={{ backgroundColor: '#343A40', borderRadius: '11px' }}
+            style={{backgroundColor: '#343A40', borderRadius: '11px'}}
             variant="contained"
             onClick={() => navigate('/login')}
           >
             Login
           </Button>
           <Button
-            style={{ backgroundColor: '#0055FF', borderRadius: '11px' }}
+            style={{backgroundColor: '#0055FF', borderRadius: '11px'}}
             variant="contained"
             onClick={() => navigate('/register')}
           >
@@ -35,10 +41,11 @@ function RegisterFormSide() {
         </div>
       </div>
 
-      <RegisterForm />
+      <RegisterForm/>
       <p className={style.form__underline}>
         Already have account
-        <span style={{ color: '#1E74FD', fontWeight: 'bold' }} onClick={() => navigate('/login')}>
+        <span style={{color: '#1E74FD', fontWeight: 'bold'}}
+              onClick={() => navigate('/login')}>
           {' '}
           Login
         </span>
@@ -47,12 +54,25 @@ function RegisterFormSide() {
   );
 }
 
+
+type Payload = {
+  username?: string
+  email?: string
+  password?: string
+  passwordConfirm?: string
+}
+
 function RegisterForm() {
   const navigate = useNavigate();
   const registerForm = useRef<null | HTMLFormElement>(null);
   const [isLoading, setLoading] = useState(false);
   const [isError, setError] = useState(false);
   const [errorText, setErrorText] = useState('Something with server was wrong');
+
+
+  const errorDisable = () => {
+    setError(false)
+  }
 
   const handleRegister = () => {
     const form = registerForm.current;
@@ -65,21 +85,43 @@ function RegisterForm() {
       setError(true);
       return;
     }
-    const payload = {
-      username: form.username.value,
-      email: form.email.value,
-      password: form.password.value,
-      passwordConfirm: form.password.value
-    };
+    const payload: Payload = {};
+    try {
+      payload.username = validateUsername(form.username.value)
+      payload.email = validateEmail(form.email.value)
+      payload.password = validatePassword(form.password.value)
+      payload.passwordConfirm = validatePassword(form.passwordConfirm.value)
+      passwordSameValidate(payload.password, payload.passwordConfirm)
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        switch (err.message) {
+          case "UsernameError":
+            setErrorText("The user field is require length must be greater then 4 and not contains space")
+            break
+          case "EmailError":
+            setErrorText("The email field should not be valid, check the correctness of filling")
+            break
+          case "PasswordError":
+            setErrorText("The password field is require length must be not less then 8 and not contains space and not be too common")
+            break
+          case "PasswordDiffError":
+            setErrorText("The password is different")
+            break
+          default:
+            setErrorText(err.message)
+        }
+        setError(true)
+      }
+      return;
+    }
     setLoading(true);
+    console.log(payload)
     axios
       .post(`${API_URL}/api/auth/register/`, payload)
       .then((response) => {
         if (response.status === 200) {
-          setTimeout(() => {
-            setLoading(false);
-            navigate('/login');
-          }, 2000);
+          setLoading(false);
+          navigate('/login');
         }
       })
       .catch((reason) => {
@@ -102,70 +144,76 @@ function RegisterForm() {
       }}
     >
       <p className={style.form__headline}>Create your account</p>
-      {isLoading && <InfinitySpin width="200" color="#4fa94d" />}
+      {isLoading && <InfinitySpin width="200" color="#4fa94d"/>}
       {isError && <Alert severity="error">{errorText}</Alert>}
       <Box
         className={style.form__box}
         sx={{
           width: '35%',
-          '& > :not(style)': { m: 1 }
+          '& > :not(style)': {m: 1}
         }}
       >
         <form ref={registerForm}>
           <Input
             placeholder="Username"
+            onChange={errorDisable}
             id="username"
             startAdornment={
               <InputAdornment position="start">
-                <AccountCircle />
+                <AccountCircle/>
               </InputAdornment>
             }
             fullWidth={true}
           />
-          <br />
+          <br/>
           <Input
             placeholder="Email"
+            onChange={errorDisable}
             id="email"
             startAdornment={
               <InputAdornment position="start">
-                <MailOutlineIcon />
+                <MailOutlineIcon/>
               </InputAdornment>
             }
             fullWidth={true}
           />
-          <br />
+          <br/>
           <Input
             placeholder="Password"
+            onChange={errorDisable}
             id="password"
             startAdornment={
               <InputAdornment position="start">
-                <LockIcon />
+                <LockIcon/>
               </InputAdornment>
             }
             type={'password'}
             fullWidth={true}
           />
-          <br />
+          <br/>
           <Input
             placeholder="Confirm password"
+            onChange={errorDisable}
             id="passwordConfirm"
             startAdornment={
               <InputAdornment position="start">
-                <LockIcon />
+                <LockIcon/>
               </InputAdornment>
             }
             type={'password'}
             fullWidth={true}
           />
-          <br />
+          <br/>
           <FormControlLabel
-            style={{ color: '#808383' }}
-            control={<Checkbox id="term" style={{ fontSize: '12px' }} name="term" />}
+            style={{color: '#808383'}}
+            control={<Checkbox onChange={errorDisable} id="term"
+                               style={{fontSize: '12px'}}
+                               name="term"/>}
             label="Accept Term and Conditions"
           />
           <Button
             onClick={handleRegister}
-            style={{ backgroundColor: '#343A40', borderRadius: '7px' }}
+            style={{backgroundColor: '#343A40', borderRadius: '7px'}}
             variant="contained"
             fullWidth={true}
           >
@@ -181,7 +229,7 @@ export function RegisterPage() {
   return (
     <main className={style.register__root}>
       <div className={style.left__side}></div>
-      <RegisterFormSide />
+      <RegisterFormSide/>
     </main>
   );
 }
