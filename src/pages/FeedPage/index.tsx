c76290841f2b9c89f26a78180329bb84377cc684
fayzikuloff.b <fayzikuloff.b@gmail.com>
2023-03-07 20:48:20 +0300

    :hammer: Customize story bar

--- src/pages/FeedPage/index.tsx
+++ src/pages/FeedPage/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { getResponse } from '../../services/utils/sendRequest';
 import { API_URL } from '../../config';
 import jwt_decode from 'jwt-decode';
-import { IFeedPost } from '../../types';
+import { IFeedPost, IFollower } from '../../types';
 import defaultUserLogo from '../../assets/images/default_user.jpg';
 import { useNavigate } from 'react-router-dom';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
@@ -25,7 +25,7 @@ const FeedItem = ({ feedPost }: { feedPost: IFeedPost }) => {
   const { username }: { username: string } = jwt_decode(
     localStorage.getItem('authToken') as string
   );
-  console.log(username);
+
   const navigate = useNavigate();
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
@@ -171,38 +171,35 @@ const FeedItems = ({ feeds }: { feeds: IFeedPost[] }) => {
 
 const FriendsList = ({ userId }: { userId: number }) => {
   const scrollRef = useRef<HTMLDivElement | null>(null);
+  const [followed, setFollowed] = useState<IFollower[]>([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getResponse(
+      `${API_URL}/api/profiles/${userId}/followed/`,
+      JSON.parse(localStorage.getItem('authToken') as string).access
+    ).then((response) => {
+      setFollowed(response.data);
+    });
+  }, []);
+
   const moveSlider = (scrollDur: number) => {
-    console.log(scrollRef);
     scrollRef.current?.scrollBy(scrollDur, 0);
   };
-  const friends = [
-    { id: 1, username: '1' },
-    { id: 2, username: '2' },
-    { id: 3, username: '3' },
-    { id: 4, username: '4' },
-    { id: 5, username: '5' },
-    { id: 6, username: '6' },
-    { id: 7, username: '7' },
-    { id: 8, username: '8' },
-    { id: 9, username: '9' },
-    { id: 10, username: '10' },
-    { id: 11, username: '11' },
-    { id: 12, username: '12' },
-    { id: 13, username: '13' },
-    { id: 14, username: '14' },
-    { id: 15, username: '15' },
-    { id: 16, username: '16' },
-    { id: 17, username: '17' },
-    { id: 18, username: '18' }
-  ];
-  console.log('User Friends ', userId);
+
   return (
     <div className={style.friend__list}>
       <div ref={scrollRef} className={style.wrapper}>
-        {friends.map((friend) => {
+        {followed.map((f) => {
           return (
-            <div className={style.story} key={friend.id}>
-              {friend.username}
+            <div className={style.story} key={f.id}>
+              <img
+                onClick={() => navigate(`/profile/${f.follower.id}`)}
+                src={f.follower.avatar ? API_URL?.concat(f.follower.avatar) : defaultUserLogo}
+                alt=""
+              />
+              <p onClick={() => navigate(`/profile/${f.follower.id}`)}>{f.follower.username}</p>
             </div>
           );
         })}
@@ -216,7 +213,6 @@ const FriendsList = ({ userId }: { userId: number }) => {
     </div>
   );
 };
-
 export function FeedPage() {
   const [feeds, setFeeds] = useState<IFeedPost[]>([]);
   const { user_id }: { user_id: number } = jwt_decode(localStorage.getItem('authToken') as string);
@@ -226,7 +222,6 @@ export function FeedPage() {
       JSON.parse(localStorage.getItem('authToken') as string).access
     )
       .then((response) => {
-        console.log(response.data);
         setFeeds(response.data);
       })
       .catch();
